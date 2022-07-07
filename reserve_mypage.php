7034227226b6e943948c2ca5f233faf705363464
MinamiInoue2323 <minami.inoue6565@gmail.com>
2022-07-08 00:37:11 +0900

    update mypage

--- reserve_mypage.php
+++ reserve_mypage.php
@@ -19,40 +19,30 @@
         $login_data = array(
             'email' => $email
         );
-        $send_url = 'http://sample.homestead.test/receive.php';
+        $send_url = 'http://localhost:8090/reservation';
         // receive.phpにJSON形式でデータを投げる
         $json = json_encode($login_data, JSON_PRETTY_PRINT);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_URL, '/reservation');
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_URL, $send_url);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         $result = curl_exec($ch);
-        $events = json_decode($result, true);
-        $events = Array(
-            "0" => Array(
-                "id" => 0,
-                "date" => 2022/01/01,
-                "event_name" => "ライブ"
-            ),
-            "1" => Array(
-                "id" => 1,
-                "date" => 2022/01/01,
-                "event_name" => "なんか"
-            )
-            );
-        $count = count($events);
+        $result = json_decode($result, true);
+        $httpcode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+        $reservations = $result['reservation'];
+        $count = count($reservations);
    
         for($i = 0 ; $i < $count ; $i ++){
             echo "<div class='card w-50 mb-3' style='margin: auto;'>";
             echo "<div class='card-body'>";
-            echo "<h2 class='card-title'>".$events[$i]["event_name"]."</h2>";
-            echo "<h3 class='card-text mb-2'>日にち：".$events[$i]["date"]."</h3>";
-            echo "<form  action='reserve_delete.php' method='get'><input type='hidden' name='event_id' value=".$events[$i]["id"]."><div class='btn btn-secondary btn-sm'><input type='submit' value='Detail'></div></form>";
+            echo "<h2 class='card-title'>".$reservations[$i]["event_name"]."</h2>";
+            echo "<h3 class='card-text mb-2'>日にち：".$reservations[$i]["day"]."</h3>";
+            echo "<form  action='reserve_delete.php' method='get'><input type='hidden' name='event_id' value=".$reservations[$i]["id"]."><div class='btn btn-secondary btn-sm'><input type='submit' value='Delete'></div></form>";
             echo "</div>";
             echo "</div>";
         }
